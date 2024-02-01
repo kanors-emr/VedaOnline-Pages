30b56e6e2aba3532e880ab5fb849421dbfe95395
kanors-himanshu03 <66825208+kanors-himanshu03@users.noreply.github.com>
2024-02-01 12:18:20 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'KanORS-EMR'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    # test comment
 ]
 
 # Add any paths that contain templates here, relative to this directory.
